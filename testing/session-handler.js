2fe771eb94fc06a97decd2cac43b4cfd5fd3b015
Pekka Lukkari <pekkaj.lukkari@gmail.com>
2016-03-10 12:12:41 +0200

    Added test file

---
+++ testing/session-handler.js
@@ -0,0 +1,33 @@
+﻿
+var SessionKasittelija = {
+    SessionStorageSet: function (key, value) {
+        if (window.sessionStorage) {
+            sessionStorage.setItem(key, value);
+        }
+    },
+    SessionStorageGet: function (key) {
+        var storageValue = null;
+    
+        if (window.sessionStorage)
+            storageValue = sessionStorage.getItem(key);
+    
+        return storageValue;
+    },
+    SessionStorageGetAndRemove: function (key) {
+        var storageValue = null;
+    
+        if (window.sessionStorage) {
+            storageValue = this.SessionStorageGet(key);
+            if (storageValue != null) {
+                this.SessionStorageRemove(key);
+            }
+        }
+    
+        return storageValue;
+    },
+    SessionStorageRemove: function (key) {
+        if (window.sessionStorage) {
+            sessionStorage.removeItem(key);
+        }
+    }
+}
